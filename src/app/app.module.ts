d75b3a135409d2e484034c6570987ac7feccf7c5
Leran Wang <leranwang10@gmail.com>
2018-04-09 22:49:51 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TitleComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
